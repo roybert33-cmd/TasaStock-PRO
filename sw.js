f5e06157cd5b01426193f41f33a92dd3dd769f8a
roybert33-cmd <roybert33@gmail.com>
2026-07-23 17:15:56 -0400

    Agregar archivo

--- sw.js
+++ sw.js
@@ -1,26 +1,17 @@
-const CACHE = "tasastock-v4-final";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+const CACHE_NAME = 'tasastock-pro-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-  self.clients.claim();
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
-self.addEventListener("fetch", e => {
-  if (e.request.method !== 'GET') return;
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(cached => {
-      const fetchPromise = fetch(e.request).then(networkResponse => {
-        if (networkResponse.ok) {
-          const clone = networkResponse.clone();
-          caches.open(CACHE).then(cache => cache.put(e.request, clone));
-        }
-        return networkResponse;
-      }).catch(() => cached);
-      return cached || fetchPromise;
-    })
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      return caches.open(CACHE_NAME).then(c => { c.put(e.request, res.clone()); return res; });
+    }).catch(()=> caches.match('./')))
   );
 });
