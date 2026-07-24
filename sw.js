6ce2e79cc956475ec0d24e8ab65ced719822b515
roybert33-cmd <roybert33@gmail.com>
2026-07-24 01:06:14 -0400

    Agregar archivo

--- sw.js
+++ sw.js
@@ -1,17 +1,37 @@
+const CACHE_NAME = "tasastock-pro-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
-const CACHE_NAME = 'tasastock-pro-v2';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
 });
-self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
-});
-self.addEventListener('fetch', (e) => {
+
+self.addEventListener("fetch", (e) => {
+  // Solo cache para GET mismo origen
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      return caches.open(CACHE_NAME).then(c => { c.put(e.request, res.clone()); return res; });
-    }).catch(()=> caches.match('./')))
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(res => {
+        // opcional: guardar nuevas respuestas
+        return res;
+      }).catch(()=> cached);
+    })
   );
 });
